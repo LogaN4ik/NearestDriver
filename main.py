e86ae34eaa918c605c6568d92ff3f01aec50e494
logan <logan4ig@gmail.com>
2022-04-12 03:51:30 +0700

    add id filter

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import requests
 import folium
 import time
 from folium.plugins import HeatMap
-#test branch. Funcs args of coord add
 #           lat             lon
 #norilsk 69.346        88.21
 #valek   69.4          88.36
@@ -21,7 +20,6 @@ def collect_data(our_lat, our_lon):
         "is_retro": True,
         "simplify": True})
     data_source = response.json()
-    print(str(len(data_source.get('drivers'))) + ' drivers') #count of nearest drivers
     data_drivers = data_source.get('drivers')
     for i in data_drivers:  #for each driver in json we will get only intereting for us info
         driver_id = i.get('id')
@@ -38,17 +36,12 @@ def collect_data(our_lat, our_lon):
                 'driver_position_timestamp': driver_position_timestamp
             }
         )
-    #with open('result.json', 'w') as file: #write drivers info into result.json
-    #    json.dump(result, file, indent=4, ensure_ascii=False)
-    #print('result= ', result)
     return result
 
 def collect_coord():
-    #with open('result.json') as file:   # read result.json
     with open('alldata.json') as file:   # read result.json
         drivers_info = json.load(file)
-    #print('all drivers info: ', drivers_info)
-    print(len(drivers_info))
+    print(len(drivers_info), "records of drivers info")
 
     lats = []
     lons = []
@@ -78,9 +71,9 @@ def main():
         for i, val in enumerate(lat_list):
             data = collect_data(lat_list[i], lon_list[i])
             all_data = all_data + data
-            print(i, lat_list[i], lon_list[i])
-            print('collect data = ', data)
-        print('all data = ',all_data)
+            print("\n #",i, "coordinate of nearest drivers: ", lat_list[i], lon_list[i])
+            print('collected data from coordinate = ', data)
+        print('\n all data = ',all_data)
 
         d = {}
         for x in all_data:
