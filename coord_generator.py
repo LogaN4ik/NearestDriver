bed0e5b6d212e8ac76c424c3e35de16b9034ab6d
logan <logan4ig@gmail.com>
2022-04-22 06:07:10 +0700

    multi geo talnakh and norilsk

---
+++ coord_generator.py
@@ -0,0 +1,93 @@
+from numpy import arange
+
+
+def get_lons(lat_start, lat_stop, lon_start, lon_stop):
+    lat_delta = round(lat_start - lat_stop, 3)
+    lon_delta = round(lon_start - lon_stop, 3)
+    lat_count = round(lat_delta / 0.005)
+    lon_count = round(lon_delta / 0.005)
+
+    # print(lat_count, 'vert line')
+    # print(lon_count, 'horizon line')
+    # print(lat_count * lon_count, 'points ')
+
+    lons = []
+    lats = []
+
+    for i in arange(lon_start, lon_stop, 0.005):
+        lon = [round(i, 3)]
+        lons = lons + lon
+
+    for i in arange(lat_start, lat_stop, 0.005):
+        lat = [round(i, 3)]
+        lats = lats + lat
+
+    # print('all lats: ', lats)
+    # print('all lons: ', lons)
+
+    lat_lon_comb = [(x, y) for x in lats for y in lons]
+    # print('list of all combinations: ', lat_lon_comb)
+
+    list_lats = []
+    for i, val in enumerate(lat_lon_comb):
+        clear_lat = ((lat_lon_comb[i])[0])
+        float_lat = float(clear_lat)
+        list_lat = [float_lat]
+        list_lats = list_lats + list_lat
+    # print(list_lats)
+
+    list_lons = []
+    for i, val in enumerate(lat_lon_comb):
+        clear_lon = ((lat_lon_comb[i])[1])
+        float_lon = float(clear_lon)
+        list_lon = [float_lon]
+        list_lons = list_lons + list_lon
+    # print(list_lons)
+    # print(list_lats[1], list_lons[1])
+    return list_lons
+
+
+def get_lats(lat_start, lat_stop, lon_start, lon_stop):
+    lat_delta = round(lat_start - lat_stop, 3)
+    lon_delta = round(lon_start - lon_stop, 3)
+    lat_count = round(lat_delta / 0.005)
+    lon_count = round(lon_delta / 0.005)
+
+    # print(lat_count, 'vert line')
+    # print(lon_count, 'horizon line')
+    # print(lat_count * lon_count, 'points ')
+
+    lons = []
+    lats = []
+
+    for i in arange(lon_start, lon_stop, 0.005):
+        lon = [round(i, 3)]
+        lons = lons + lon
+
+    for i in arange(lat_start, lat_stop, 0.005):
+        lat = [round(i, 3)]
+        lats = lats + lat
+
+    # print('all lats: ', lats)
+    # print('all lons: ', lons)
+
+    lat_lon_comb = [(x, y) for x in lats for y in lons]
+    # print('list of all combinations: ', lat_lon_comb)
+
+    list_lats = []
+    for i, val in enumerate(lat_lon_comb):
+        clear_lat = ((lat_lon_comb[i])[0])
+        float_lat = float(clear_lat)
+        list_lat = [float_lat]
+        list_lats = list_lats + list_lat
+    # print(list_lats)
+
+    list_lons = []
+    for i, val in enumerate(lat_lon_comb):
+        clear_lon = ((lat_lon_comb[i])[1])
+        float_lon = float(clear_lon)
+        list_lon = [float_lon]
+        list_lons = list_lons + list_lon
+    # print(list_lons)
+    # print(list_lats[1], list_lons[1])
+    return list_lats
